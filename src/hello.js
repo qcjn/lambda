f9e0cbbf32c9811ab14207683e5e8625b708e156
qcjn <40976058+qcjn@users.noreply.github.com>
2020-01-12 16:50:01 +0800

    update

--- src/hello.js
+++ src/hello.js
@@ -1,8 +1,6 @@
 export async function handler(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello world!`
-    })
+    body: JSON.stringify(event, null, 2)
   }
 }
